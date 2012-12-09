9979fbb8c7604ee9a3605f865cd5929c4dcf5de1
Ken Pepple <ken.pepple@cloudtp.com>
2012-12-09 13:35:47 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mtbcalendar::Application.config.secret_token = 'c6b94a2f13b64a28fce8464aa65a6962eaca1c2c2a08a4f4ac380c74978eb037f2b1de29a81ac24fae8e56c6998d63084c64977bfefa8cfa9fd8a0b2014dcc0d'
